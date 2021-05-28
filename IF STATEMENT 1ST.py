9662dd1c919437fea68d44b91b3673f6ef56eee2
RishavRoyChowdhury <84906716+RishavRoyChowdhury@users.noreply.github.com>
2021-05-28 16:06:18 +0530

    Add files via upload

---
+++ IF STATEMENT 1ST.py
@@ -0,0 +1,9 @@
+print("WELCOME TO INTEGER CHARETERISTIC APP 'ROX'.DEVELOPED BY RISHAV ROY CHOWDHURY.")
+Y = input("enter a INTEGER= ")
+if int(Y) < 0:
+     print("NEGATIVE")
+if int(Y) > 0:
+     print("POSITIVE")
+if int(Y) == 0:
+    print("THAT'S 0 BRO!")
+print("FINISH!")     
