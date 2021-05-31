181e4738c04d23e0ebe13b461bdb29505868333b
RishavRoyChowdhury <84906716+RishavRoyChowdhury@users.noreply.github.com>
2021-05-31 20:27:06 +0530

    Add files via upload

--- IF STATEMENT 1ST.py
+++ IF STATEMENT 1ST.py
@@ -1,9 +1,22 @@
-print("WELCOME TO INTEGER CHARETERISTIC APP 'ROX'.DEVELOPED BY RISHAV ROY CHOWDHURY.")
-Y = input("enter a INTEGER= ")
-if int(Y) < 0:
+print("WELCOME TO NUMBER CHARETERISTIC APP 'ROX'.DEVELOPED BY RISHAV ROY CHOWDHURY.")
+x = input("WRITE YOUR NAME AND PRESS ENTER: ")
+print("HELLO",x )
+def ui():
+  print("FINISH!WILL YOU CONSIDER RATING ME ON GitHub?")
+def yu():
+  Y = input("ENTER A NUMBER= ")
+  try :
+    if float(Y) < 0:
      print("NEGATIVE")
-if int(Y) > 0:
+     ui()
+    if float(Y) > 0:
      print("POSITIVE")
-if int(Y) == 0:
-    print("THAT'S 0 BRO!")
-print("FINISH!")     
+     ui()
+    if float(Y) == 0:
+     print("THAT'S 0 BRO!")
+     ui()
+  except :
+     print("I DON'T MAKE OUT WHAT YOU ARE SAYING ;<")     
+  x = input("PRESS 'ENTER' TO USE AGAIN")
+while str(x):
+  yu()
